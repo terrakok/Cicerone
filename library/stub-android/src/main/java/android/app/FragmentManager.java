16711e4fbb9ac955e0ad6db6de92b2dd6332bfbf
Chad Bingham <Chad.Bingham52@gmail.com>
2017-03-27 19:51:02 -0600

    Spaces > Tabs

--- library/stub-android/src/main/java/android/app/FragmentManager.java
+++ library/stub-android/src/main/java/android/app/FragmentManager.java
@@ -5,37 +5,37 @@ package android.app;
  */
 public class FragmentManager {
 
-	public FragmentTransaction beginTransaction() {
-		throw new RuntimeException("Stub!");
-	}
+    public FragmentTransaction beginTransaction() {
+        throw new RuntimeException("Stub!");
+    }
 
-	public boolean executePendingTransactions() {
-		throw new RuntimeException("Stub!");
-	}
+    public boolean executePendingTransactions() {
+        throw new RuntimeException("Stub!");
+    }
 
-	public void popBackStack() {
-		throw new RuntimeException("Stub!");
-	}
+    public void popBackStack() {
+        throw new RuntimeException("Stub!");
+    }
 
-	public boolean popBackStackImmediate() {
-		throw new RuntimeException("Stub!");
-	}
+    public boolean popBackStackImmediate() {
+        throw new RuntimeException("Stub!");
+    }
 
-	public boolean popBackStackImmediate(String name, int flags) {
-		throw new RuntimeException("Stub!");
-	}
+    public boolean popBackStackImmediate(String name, int flags) {
+        throw new RuntimeException("Stub!");
+    }
 
-	public int getBackStackEntryCount() {
-		throw new RuntimeException("Stub!");
-	}
+    public int getBackStackEntryCount() {
+        throw new RuntimeException("Stub!");
+    }
 
-	public BackStackEntry getBackStackEntryAt(int index) {
-		throw new RuntimeException("Stub!");
-	}
+    public BackStackEntry getBackStackEntryAt(int index) {
+        throw new RuntimeException("Stub!");
+    }
 
-	public interface BackStackEntry {
-		int getId();
+    public interface BackStackEntry {
+        int getId();
 
-		String getName();
-	}
+        String getName();
+    }
 }
