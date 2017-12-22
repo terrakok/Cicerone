e1a2f7d681ac06be923fa1d45683f09f61182352
OsipXD <osip.fatkullin@gmail.com>
2017-12-22 14:08:29 +0300

    Convert commands to Kotlin

---
+++ library/src/main/java/ru/terrakok/cicerone/commands/Commands.kt
@@ -0,0 +1,49 @@
+package ru.terrakok.cicerone.commands
+
+
+/**
+ * Rolls back the last transition from the screens chain.
+ *
+ * @author Konstantin Tskhovrebov (aka terrakok) on 11.10.16.
+ */
+class Back : Command
+
+
+/**
+ * Rolls back to the needed screen from the screens chain.
+ * Behavior in the case when no needed screens found depends on an implementation of the [ru.terrakok.cicerone.Navigator].
+ * But the recommended behavior is to return to the root.
+ *
+ * @param screenKey screen key or null if you need back to root screen
+ * @author Konstantin Tskhovrebov (aka terrakok) on 04.07.17.
+ */
+class BackTo(val screenKey: String?) : Command
+
+
+/**
+ * Opens new screen.
+ *
+ * @param screenKey      screen key
+ * @param transitionData initial data, can be null
+ * @author Konstantin Tskhovrebov (aka terrakok) on 11.10.16.
+ */
+class Forward(val screenKey: String, val transitionData: Any?) : Command
+
+
+/**
+ * Replaces the current screen.
+ *
+ * @param screenKey      screen key
+ * @param transitionData initial data, can be null
+ * @author Konstantin Tskhovrebov (aka terrakok) on 11.10.16.
+ */
+class Replace(val screenKey: String, val transitionData: Any?) : Command
+
+
+/**
+ * Shows system message.
+ *
+ * @param message message text
+ * @author Konstantin Tskhovrebov (aka terrakok) on 11.10.16.
+ */
+class SystemMessage(val message: String) : Command
