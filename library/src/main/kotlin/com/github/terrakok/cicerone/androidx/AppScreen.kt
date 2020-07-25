4872d10d6a555f6cb8cb279c7b896b424551d61f
terrakok <terrakok@gmail.com>
2020-07-26 00:39:01 +0300

    Add screen keys and update version to 6.0.3-dev

--- library/src/main/kotlin/com/github/terrakok/cicerone/androidx/AppScreen.kt
+++ library/src/main/kotlin/com/github/terrakok/cicerone/androidx/AppScreen.kt
@@ -10,10 +10,12 @@ import com.github.terrakok.cicerone.Screen
 sealed class AppScreen : Screen()
 
 open class FragmentScreen(
+    override val screenKey: String,
     val createFragment: (FragmentFactory) -> Fragment
 ) : AppScreen()
 
 open class ActivityScreen(
+    override val screenKey: String,
     val createIntent: (context: Context) -> Intent
 ) : AppScreen() {
     open val startActivityOptions: Bundle? = null
