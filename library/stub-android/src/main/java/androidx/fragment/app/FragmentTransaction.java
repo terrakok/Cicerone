d8ee40c0eacea7f27bafe0ac961b6d4e477fa81e
terrakok <terrakok@gmail.com>
2020-10-21 16:57:26 +0300

    Add screenKey to fragment transaction tag.

--- library/stub-android/src/main/java/androidx/fragment/app/FragmentTransaction.java
+++ library/stub-android/src/main/java/androidx/fragment/app/FragmentTransaction.java
@@ -1,32 +1,16 @@
 package androidx.fragment.app;
 
-import android.os.Bundle;
-
 /**
  * Created by Konstantin Tskhovrebov (aka @terrakok)
  * on 11.10.16
  */
 
 public class FragmentTransaction {
-    public FragmentTransaction add(int containerViewId, Fragment fragment) {
+    public FragmentTransaction add(int containerViewId, Fragment fragment, String tag) {
         throw new RuntimeException("Stub!");
     }
 
-    public final FragmentTransaction add(
-            int containerViewId,
-            Class<? extends Fragment> fragmentClass,
-            Bundle args) {
-        throw new RuntimeException("Stub!");
-    }
-
-    public FragmentTransaction replace(int containerViewId, Fragment fragment) {
-        throw new RuntimeException("Stub!");
-    }
-
-    public final FragmentTransaction replace(
-            int containerViewId,
-            Class<? extends Fragment> fragmentClass,
-            Bundle args) {
+    public FragmentTransaction replace(int containerViewId, Fragment fragment, String tag) {
         throw new RuntimeException("Stub!");
     }
 
